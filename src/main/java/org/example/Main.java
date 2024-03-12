3356f5504ab465bd2647e8880797bf1684a62418
basti <bastian_behrendt@yahoo.de>
2024-03-12 15:22:55 +0100

    new methods: - checks the password length - checks if the password contains digits - checks if the password contains upper and lowercase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,46 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(checkIfPasswordContainsDigits("tes2%t"));
+
     }
+
+
+    public static String checkPasswordLength(String password) {
+        if(password.length()< 8){
+            return "Password muss min. 8 Zeichen enthalten!";
+        }
+            return "Tolles Passwort!";
+    }
+
+    public static String checkIfPasswordContainsDigits(String password) {
+
+        for(char c : password.toCharArray()){
+            if(Character.isDigit(c)){
+                return "Tolles Passwort!";
+            }
+        }
+                return "Keine Sonderzeichen gefunden";
+    }
+
+    public static String checkIfPasswordContainsUpperAndLowerCase(String password){
+        boolean containsUpperCase = false;
+        boolean containsLowerCase = false;
+
+        for (char c : password.toCharArray()){
+            if(Character.isLowerCase(c)){
+                containsLowerCase = true;
+            }
+            if(Character.isUpperCase(c)){
+                containsUpperCase = true;
+            }
+        }
+            if(containsLowerCase && containsUpperCase){
+                return "Tolles Passwort!";
+            }else{
+                return "Das Passwort beinhaltet nur upper oder lowercase";
+            }
+
+    }
+
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    //GENAU 8 Chars
+    @Test
+    void checkPasswordLength_ShouldReturnStringTollesPasswort_perfectAmountOfChars() {
+        //GIVEN
+        String expected = "Tolles Passwort!";
+        //WHEN
+        String actualy;
+        actualy = Main.checkPasswordLength("t2312454");
+        //THEN
+        Assertions.assertEquals(expected, actualy);
+    }
+
+    // zu wenig Zeichen
+    @Test
+    void checkPasswordLength_ShouldReturnStringNotEnoughChars_LessThen8Chars() {
+        //GIVEN
+        String expected = "Password muss min. 8 Zeichen enthalten!";
+        //WHEN
+        String actualy;
+        actualy = Main.checkPasswordLength("t231245");
+        //THEN
+        Assertions.assertEquals(expected, actualy);
+    }
+
+    // Mehr als 8 Chars
+    @Test
+    void checkPasswordLength_ShouldReturnStringEnoughChars_MoreThen8Chars() {
+        //GIVEN
+        String expected = "Password muss min. 8 Zeichen enthalten!";
+        //WHEN
+        String actualy;
+        actualy = Main.checkPasswordLength("t231245");
+        //THEN
+        Assertions.assertEquals(expected, actualy);
+    }
+
+    @Test
+    void checkIfPasswordContainsDigits_ShouldReturnExpected_WithDigits() {
+        //GIVEN
+        String expected = "Tolles Passwort!";
+        //WHEN
+        String actualy;
+        actualy = Main.checkIfPasswordContainsDigits("password2");
+        //THEN
+        Assertions.assertEquals(expected, actualy);
+    }
+    @Test
+    void checkIfPasswordContainsUpperAndLowerCase_ShouldReturnExpected_WithUpperAndLower() {
+        //GIVEN
+        String expected = "Tolles Passwort!";
+        //WHEN
+        String actualy;
+        actualy = Main.checkIfPasswordContainsUpperAndLowerCase("passwordS2");
+        //THEN
+        Assertions.assertEquals(expected, actualy);
+    }
+}
